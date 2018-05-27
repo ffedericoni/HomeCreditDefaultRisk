899c1af4388a21834daa7ff1725292a16560dc81
Fabrizio Federiconi <github.com@federiconi.com>
2018-05-27 23:26:18 +0200

    POS_CASH transformation added

--- HCDR.py
+++ HCDR.py
@@ -76,14 +76,15 @@ gc.collect()
 printlog(bureau_df.head(), "INFOBOX")
 
 # In[30]:
-print('Merge bureaus...')
-buro_full = bureau_df.merge(right=bureau_balance_df.reset_index(), how='left', on='SK_ID_BUREAU', suffixes=('', '_bur_bal'))
+printlog('Merge bureaus...')
+buro_full = bureau_df.merge(right=bureau_balance_df.reset_index(), how='left', on='SK_ID_BUREAU', 
+                            suffixes=('', '_bur_bal'))
 
-print('Counting buro per SK_ID_CURR')
+printlog('Counting buro per SK_ID_CURR')
 nb_bureau_per_curr = buro_full[['SK_ID_CURR', 'SK_ID_BUREAU']].groupby('SK_ID_CURR').count()
 buro_full['SK_ID_BUREAU_cnt'] = buro_full['SK_ID_CURR'].map(nb_bureau_per_curr['SK_ID_BUREAU'])
 
-print('Averaging bureau')
+printlog('Averaging bureau')
 avg_buro = buro_full.groupby('SK_ID_CURR').mean()
 printlog(avg_buro.head(), "INFOBOX")
 
@@ -93,7 +94,35 @@ for col in avg_buro.columns:
 del buro_full, bureau_df, bureau_balance_df
 gc.collect()
 # In[ ]:
+def prevapp_transform(df):
+    printlog('Process previous applications...')
+    prev_cat_features = [f_ for f_ in df.columns if df[f_].dtype == 'object']
+    prev_dum = pd.DataFrame()
+    for f_ in prev_cat_features:
+        prev_dum = pd.concat([prev_dum, pd.get_dummies(df[f_], prefix=f_).astype(np.uint8)], axis=1)
+    df = pd.concat([df, prev_dum], axis=1)
+    nb_prev_per_curr = df[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+    df['SK_ID_PREV_cnt'] = df['SK_ID_CURR'].map(nb_prev_per_curr['SK_ID_PREV'])
+    df = df.groupby('SK_ID_CURR').mean()
+    return df
 
+prev_app_df = prevapp_transform(prev_app_df)
+gc.collect()
+printlog(prev_app_df.head(), "INFOBOX")
+#%%
+def poscash_transform(df):
+    printlog('Process POS_CASH...')
+    df = pd.concat([df, pd.get_dummies(df['NAME_CONTRACT_STATUS'])], axis=1)
+    nb_prevs = df[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+    df['SK_ID_PREV_cnt'] = df['SK_ID_CURR'].map(nb_prevs['SK_ID_PREV'])
+    df = df.groupby('SK_ID_CURR').mean()
+    return df
+
+pos_cash_df = poscash_transform(pos_cash_df)
+gc.collect()
+printlog(pos_cash_df.head(), "INFOBOX")
+
+#%%
 def feature_engineering(app_data, bureau_df, bureau_balance_df, credit_card_df,
                         pos_cash_df, prev_app_df, install_df):
     """ Process the dataframes into a single one containing all the transformed features """
